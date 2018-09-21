25cface4d16920a5e5f2d59bd2a2ce796a6c10ea
SQSong <SQSong66@163.com>
2018-09-21 08:54:06 +0800

    init commit.

---
+++ app/src/main/java/com/sqsong/wanandroid/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.sqsong.wanandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
